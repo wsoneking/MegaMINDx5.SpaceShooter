d1fbaf5ab511f6c82f295cc5a91e45d52933dc76
Jin Wang <jin.wang@pearson.com>
2014-08-27 12:41:32 -0600

    Update all

---
+++ Assets/Scripts/DistroyByContact.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistroyByContact : MonoBehaviour {
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	public int scoreValue;
+	public GameController gameController;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+		if (gameControllerObject != null) 
+		{
+			gameController = gameControllerObject.GetComponent <GameController>(); 
+		}
+		if (gameControllerObject == null)
+		{
+			Debug.Log("Can't find GameController");
+		}
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
+		{
+			return;
+		}
+		if (other.tag == "Player") 
+		{
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver ();
+		}
+
+		Instantiate (explosion, transform.position, transform.rotation);
+
+		gameController.AddScore (scoreValue);
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+public class Boundary
+{
+	public float Xmin, Xmax, Zmin, Zmax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public Boundary boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	void Update()
+	{
+		if ((Input.GetKey ("space")||Input.GetButton ("Fire1")) && Time.time > nextFire) {
+			nextFire = Time.time + fireRate ;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			audio.Play ();
+		}
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.velocity = movement * speed; 
+
+		rigidbody.position = new Vector3 (
+			Mathf.Clamp (rigidbody.position.x, boundary.Xmin, boundary.Xmax),
+			0.0f,
+			Mathf.Clamp (rigidbody.position.z, boundary.Zmin, boundary.Zmax)
+		   );
+		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+	 }
+}
